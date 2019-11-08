3b07b21bcccfe8d9c66ade78d0d6dfc1bc9e4e85
benlegendj <benlegendj@gmail.com>
2019-11-09 00:50:10 +0300

    expressions and conditionals in jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './styles/main.css';
+import { conditionalExpression } from '@babel/types';
 
 class App extends Component {
 	render() {
+    const user = 'mwendwa';
+    const loading = true;
+    const showName = false;
 		return (
-			<div className='App'>
-				<h1>Hello World</h1>
-			</div>
+			// fragment --> let you group a list of children without adding extra nodes to the DOM.
+			<Fragment className='App'>
+				<h1>Hello {user}</h1>
+        {user.toUpperCase()}
+        {/**  conditionalExpression using &&*/}
+        <h1>{showName&& user}</h1>
+			</Fragment>
 		);
 	}
 }
