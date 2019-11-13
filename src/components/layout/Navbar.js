4c9561a52bd9d78006a2631a1b26d32ad2c73bfb
benlegendj <benlegendj@gmail.com>
2019-11-13 23:44:56 +0300

    use functional components and set propTypes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-export default class Navbar extends Component {
-	static defaultProps = {
-		title: 'Github-finder',
-		icon: 'fab fa-github',
-	};
-	// proptypes gives warnings when props parsedin are not of the expected type
-	static propTypes = {
-		title: PropTypes.string.isRequired,
-		icon: PropTypes.string.isRequired,
-	};
-	render() {
-		return (
-			<nav className='navbar bg-primary'>
-				<h1>
-					<i className={this.props.icon} />
-					{this.props.title}
-				</h1>
-			</nav>
-		);
-	}
-}
+
+const Navbar = ({icon,title}) => {
+	// destructure icon and title from props
+	return (
+		<nav className='navbar bg-primary'>
+			<h1>
+				<i className={icon} />
+				{title}
+			</h1>
+		</nav>
+	);
+};
+
+Navbar.defaultProps = {
+	title: 'Github-finder',
+	icon: 'fab fa-github',
+};
+
+// proptypes gives warnings when props parsedin are not of the expected type
+Navbar.propTypes = {
+	title: PropTypes.string.isRequired,
+	icon: PropTypes.string.isRequired,
+};
+
+export default Navbar;
