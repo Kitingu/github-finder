95005de5f410e3e9336e959e04bec09d4a1f7cfa
benlegendj <benlegendj@gmail.com>
2019-11-14 00:58:44 +0300

    register app to use unlimited requests and set env variables

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ class App extends Component {
 			loading: true,
 		});
 
-		const res = await axios.get('https://api.github.com/users');
+		const res = await axios.get(
+			`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
 
 		this.setState({
 			users: res.data,
@@ -25,7 +27,10 @@ class App extends Component {
 			<div className='App'>
 				<Navbar />
 				<div className='container'>
-					<Users loading={this.state.loading} users={this.state.users} />
+					<Users
+						loading={this.state.loading}
+						users={this.state.users}
+					/>
 				</div>
 			</div>
 		);
