4bb980819115916ed34b29a6b68e1b8dc9f8af6e
benlegendj <benlegendj@gmail.com>
2019-11-15 02:06:37 +0300

    add basic app routing

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Navbar = ({icon,title}) => {
+const Navbar = ({ icon, title }) => {
 	// destructure icon and title from props
 	return (
 		<nav className='navbar bg-primary'>
@@ -9,6 +10,15 @@ const Navbar = ({icon,title}) => {
 				<i className={icon} />
 				{title}
 			</h1>
+			<ul>
+				<li>
+				{/**use of Link makes sure browser does not refresh as opposed to a normal a tag */}
+					<Link to='/'>Home</Link>
+				</li>
+				<li>
+					<Link to='/about'>About</Link>
+				</li>
+			</ul>
 		</nav>
 	);
 };
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+	return (
+		<Fragment>
+			<h1>About this App</h1>
+			<h3>App to search github users</h3>
+			<p>Version:1.0.0</p>
+		</Fragment>
+	);
+};
+
+export default About;
