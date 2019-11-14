a58bd23cc6d644aecd5d1c31a42d130c614533d7
benlegendj <benlegendj@gmail.com>
2019-11-15 00:04:48 +0300

    add search functionality

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
 	state = {
 		text: '',
 	};
+
+	static propTypes = {
+		searchUsers: PropTypes.func.isRequired,
+	};
+
 	onChange = (e) => {
 		this.setState({
 			text: e.target.value,
@@ -11,6 +17,10 @@ class Search extends Component {
 	};
 	onSubmit = (e) => {
 		e.preventDefault();
+		this.props.searchUsers(this.state.text);
+		this.setState({
+			text: '',
+		});
 	};
 	render() {
 		return (
